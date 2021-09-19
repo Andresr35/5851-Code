7b6e463d032d47aa40c73fdf16e7e65505ba7271
Andres Ruiz <Andres.ruiz3561@gmail.com>
2021-09-18 20:21:12 -0700

    push

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -12,6 +12,7 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
+// this is the constants class
 public final class Constants {
 
         public static int
